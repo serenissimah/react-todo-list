dfd57baeec007789df749c9fdd5257776fb79819
Serena Hong <serenahong.usa@gmail.com>
2019-06-05 21:24:02 -0600

    lesson completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, } from 'react';
+import List from "./List";
+import TodoForm from "./TodoForm";
+import Footer from './Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  state = { todos: [] }
+
+  setFilter = (filter) => {
+    this.setState({ filter })
+  }
+
+  getUniqId = () => {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+
+  addItem = (name) => {
+    const { todos } = this.state;
+    const todo = { name, id: this.getUniqId() , complete: false }
+    this.setState({ todos: [todo, ...todos] }); 
+    }
+
+  handleClick = (id) => {
+    const { todos } = this.state;
+    this.setState({
+      todos: todos.map( todo => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            complete: !todo.complete
+          }
+        }
+        return todo
+      })
+    })
+  } 
+
+  visibleItems = () => {
+    const { todos, filter } = this.state;
+    switch(filter) {
+      case 'Active':
+        return todos.filter( t => !t.complete )
+      case 'Complete':
+        return todos.filter( t=> t.complete )
+      default:
+        return todos;
+    }
+  }
+  
+ 
+  render() {
+    const { todos, filter } = this.state;
+
+    return (
+    <div>
+    <TodoForm addItem={this.addItem} />
+    <List name="Todo List" items={this.visibleItems()} todoClick={this.handleClick} />
+    <Footer filter={filter} setFilter={this.setFilter} />
     </div>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ src/List.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Todo from "./Todo"
+
+const List = ({ items, name, todoClick }) => (
+ <div>
+   <h2>{name}</h2>
+   <ul>
+   { items.map( item => <Todo key={item.id} {...item} todoClick={todoClick} /> )}
+   </ul>
+ </div>
+)
+
+
+export default List;
